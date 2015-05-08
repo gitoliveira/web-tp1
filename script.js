ed36720af0743ee801ab5980a8aa6f11b01d80bc
gitoliveira <lovebus@Igors-MacBook-Air.local>
2015-05-07 21:46:59 -0300

    Tentativa de Parallax

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function abrirModal(){
 function abrirModal2(){
 	document.getElementById("myModal2").showModal();
 }
+
+(function(){
+
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "90% " + (windowYOffset * speed) + "px";
+
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
+
+})();
